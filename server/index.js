3d3bd2e9d41098ff32d47ebf2e040bbd23ca3d16
Alyssa Leonard <leonard.alyssa@me.com>
2018-01-20 12:34:31 -0700

    New index.js

--- server/index.js
+++ server/index.js
@@ -2,17 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const massive = require('massive');
+const shelfBaseUrl = '/api/shelf';
+const binBaseUrl = '/api/bin';
+const bc = require('./bins_controller');
+const sc = require('./shelves_controller');
+const path = require('path');
 require('dotenv').config()
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-massive(process.env.CONNECTION_STRING)
-    .then(dbInstance => {
-        app.set('db', dbInstance)
-    });
+app.use(express.static(path.resolve(__dirname, "client", "build")));  
+app.get("/", (req, res) => {  
+ res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+}); 
+massive(process.env.CONNECTION_STRING).then(dbInstance => {app.set('db', dbInstance)});
+
+app.get(`${shelfBaseUrl}/:id`, sc.getShelves);
+
+app.post(`${binBaseUrl}/:id`, bc.createBin);
+app.get(`${binBaseUrl}/:id`, bc.getBin);
+app.put(`${binBaseUrl}/:id`, bc.updateBin);
+app.delete(`${binBaseUrl}/:id`, bc.deleteBin);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-    console.log(`Welcome to the Big Show on Port ${port}!`);
+   console.log(`Welcome to the Big Show on Port ${port}!`);
 })
